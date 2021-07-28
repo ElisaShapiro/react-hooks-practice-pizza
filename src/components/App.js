d132774fe6d8f87847a5bdf15f1967d173504c89
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-28 15:11:15 -0400

    rendered pizzas

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+    .then(response => response.json())
+    .then(pizzaData => setPizzas(pizzaData))
+  }, []);
+
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzas={pizzas}/>
     </>
   );
 }
